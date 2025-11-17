92fc07a196d1845e36f2d32ddf84d98575643d8c
Paul Jenkins <paul@turnpiece.com>
2025-11-17 09:56:05 +0000

    Bump version to 1.0.1

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'ef86d8b94c269548af41');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'aa0474033bd90b4e3509');
